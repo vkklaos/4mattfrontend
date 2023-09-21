caeb7223eab59e137b8846e4d2833d1d7a5d8603
vkklaos <solaktuto@gmail.com>
2023-09-21 19:57:58 -0300

    image fix #3

--- src/Views/Components/CategorySection/CategorySection.js
+++ src/Views/Components/CategorySection/CategorySection.js
@@ -11,7 +11,7 @@ const CategorySection = () => {
     return (
     <div className={classes.container}>
         {categoryArray && categoryArray.length !== 0 ? categoryArray.map((item, index) => (
-            <CategoryCard image={`svgs/${item.category.replace(/\W+/g, '').toLowerCase()}.svg`} category={item.category} spend={item.spend} key={index} />
+            <CategoryCard image={`svgs/${item.category.replace(/\W+/g, '-').toLowerCase()}.svg`} category={item.category} spend={item.spend} key={index} />
         )) : 
             <div style={{paddingTop: '40px', width: '100%', display: 'flex', justifyContent: 'center'}}>
               <CardPlaceholder content={'No spend in selected period'} />
